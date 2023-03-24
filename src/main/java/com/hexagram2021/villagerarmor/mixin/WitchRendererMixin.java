37f219fe28c91032edf4dead2a39dd27bfee8def
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-03-24 17:33:01 +0800

    给女巫也整一套！

---
+++ src/main/java/com/hexagram2021/villagerarmor/mixin/WitchRendererMixin.java
@@ -0,0 +1,21 @@
+package com.hexagram2021.villagerarmor.mixin;
+
+import com.hexagram2021.villagerarmor.client.HumanoidArmorLayer;
+import com.hexagram2021.villagerarmor.client.models.VillagerArmorModel;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.WitchRenderer;
+import net.minecraft.client.renderer.entity.layers.ElytraLayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WitchRenderer.class)
+public class WitchRendererMixin {
+	@Inject(method = "<init>", at = @At(value = "RETURN"))
+	public void addWitchArmorLayer(EntityRendererManager rendererManager, CallbackInfo ci) {
+		WitchRenderer current = ((WitchRenderer)(Object)this);
+		current.addLayer(new HumanoidArmorLayer<>(current, new VillagerArmorModel(0.5F), new VillagerArmorModel(1.0F)));
+		current.addLayer(new ElytraLayer<>(current));
+	}
+}
